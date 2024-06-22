5df17c9e1a6c2b53065bc5457803a89b345e7afb
Jeet Bharti <144922291+JeetBharti@users.noreply.github.com>
2024-06-22 19:28:32 +0530

    SmartContract.sol

--- SmartContract.sol
+++ SmartContract.sol
@@ -1,1 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
 
+contract SimpleContractProject {
+    address owner;
+    uint private value;   ///private
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function setValue(uint _value) public {  //setup value
+        require(msg.sender == owner, "Only the owner can set the value");
+        value = _value;
+    }
+
+    function getValue() public view returns (uint) {   //getting value
+        return value;
+    }
+
+    function restrictedFunction() public view {         //restricted function  
+        require(msg.sender == owner, "Only the owner can access this function");
+
+        if (value == 0) {
+            revert("Stored value cannot be zero");
+        }
+    }
+
+    function assertTest() public pure {         //assert test
+        uint testValue = 1;
+        assert(testValue == 1); // This should always be true
+    }
+}
